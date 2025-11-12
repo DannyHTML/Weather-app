7d1d49cccdbcfb459b603b3634f4f26754c66954
Danny <dannyjag@gmail.com>
2025-11-12 02:02:36 +0000

    Some good progress and a todo for the imperial button click logic

--- src/components/DropDownSelect.vue
+++ src/components/DropDownSelect.vue
@@ -1,14 +1,19 @@
 <template>
     <template v-if="options.length > 0">
-        <div class="mt-1 not-last:border-b">
+        <div class="py-2 not-last:border-b">
             <span class="block px-2 text-neutral-400">{{ title }}</span>
 
             <template v-for="option in options" :key="option">
                 <button
-                    class="flex w-full items-center justify-between rounded-md px-2 hover:bg-neutral-600"
+                    type="button"
+                    :class="[
+                        'flex w-full cursor-pointer items-center justify-between rounded-md px-2 duration-100 hover:bg-neutral-600',
+                        selectedOption === option ? 'bg-neutral-600' : '',
+                    ]"
+                    @click="emit('update:selectedOption', option)"
                 >
                     <span>{{ option }}</span>
-                    <template v-if="isSelected">
+                    <template v-if="selectedOption === option">
                         <img src="/src/assets/images/icon-checkmark.svg" alt="checkmark" />
                     </template>
                 </button>
@@ -21,17 +26,21 @@
 defineProps({
     title: {
         type: String,
-        required: true,
+        required: false,
+        default: '',
     },
     options: {
-        type: Array,
+        type: Array as PropType<string[]>,
         required: false,
         default: () => [],
     },
-    isSelected: {
-        type: Boolean,
-        required: false,
-        default: false,
+    selectedOption: {
+        type: String,
+        required: true,
     },
 });
+
+const emit = defineEmits<{
+    (event: 'update:selectedOption', option: string): void;
+}>();
 </script>
--- src/components/header/DropDownBody.vue
+++ src/components/header/DropDownBody.vue
@@ -1,35 +1,76 @@
 <template>
     <div class="relative text-white">
-        <div
+        <button
+            type="button"
+            @click="toggleDropdown"
+            @mouseover="isOpen = true"
             class="flex w-fit cursor-pointer items-center justify-between gap-2 rounded-md bg-neutral-600 px-2 py-1"
         >
-            <img src="/src/assets/images/icon-units.svg" alt="" />
+            <img src="/src/assets/images/icon-units.svg" alt="icon-units" />
             <span class="block">Units</span>
-            <img src="/src/assets/images/icon-dropdown.svg" alt="" />
-        </div>
+            <img
+                src="/src/assets/images/icon-dropdown.svg"
+                alt="icon-dropdown"
+                :class="['duration-150', isOpen ? 'rotate-180' : '']"
+            />
+        </button>
 
-        <div class="absolute right-0 mt-2 w-56">
-            <!-- Dropdown content to be added here -->
-            <div class="rounded-md bg-neutral-700 px-3 py-2">
-                <button
-                    type="button"
-                    class="w-full cursor-pointer rounded-md px-2 text-left hover:bg-neutral-600"
-                >
-                    Switch to Imperial
-                </button>
+        <transition
+            enter-active-class="transition ease-out duration-200"
+            enter-from-class="opacity-0 -translate-y-2"
+            enter-to-class="opacity-100 translate-y-0"
+            leave-active-class="transition ease-in duration-150"
+            leave-from-class="opacity-100 translate-y-0"
+            leave-to-class="opacity-0 -translate-y-2"
+        >
+            <div v-if="isOpen" class="absolute right-0 mt-2 w-56" @mouseleave="isOpen = false">
+                <!-- Dropdown content to be added here -->
+                <div class="rounded-md bg-neutral-700 px-3 py-2">
+                    <!-- LOGIC FOR IMPERIAL BUTTON CLICK -->
+                    <button
+                        type="button"
+                        class="w-full cursor-pointer rounded-md px-2 text-left duration-150 hover:bg-neutral-600"
+                    >
+                        Switch to Imperial
+                    </button>
 
-                <DropDownSelect title="Temperature" :options="temperatureOptions" />
+                    <DropDownSelect
+                        title="Temperature"
+                        :options="temperatureOptions"
+                        v-model:selectedOption="selectedTemperature"
+                    />
+                    <DropDownSelect
+                        title="Wind Speed"
+                        :options="windSpeedOptions"
+                        v-model:selectedOption="selectedWindSpeed"
+                    />
 
-                <DropDownSelect title="Wind Speed" />
-
-                <DropDownSelect title="Precipitation" />
+                    <DropDownSelect
+                        title="Precipitation"
+                        :options="precipitationOptions"
+                        v-model:selectedOption="selectedPrecipitation"
+                    />
+                </div>
             </div>
-        </div>
+        </transition>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import DropDownSelect from '../DropDownSelect.vue';
 
 const temperatureOptions = ['Celsius', 'Fahrenheit'];
+const windSpeedOptions = ['km/h', 'mph'];
+const precipitationOptions = ['Millimeters (mm)', 'inches (in)'];
+
+const selectedTemperature = ref('');
+const selectedWindSpeed = ref('');
+const selectedPrecipitation = ref('');
+
+const isOpen = ref(false);
+
+const toggleDropdown = () => {
+    isOpen.value = !isOpen.value;
+};
 </script>
