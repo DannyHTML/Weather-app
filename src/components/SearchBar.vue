f637543887384efd2a880e8910821997d83d262b
Danny <118465352+DannyHTML@users.noreply.github.com>
2025-11-13 22:46:20 +0100

    Create search bar and title (#6)
    * Created the mobile layout for the searchBar and the title. Also added the button but still needs its func...
    
    * Layout on desktop, not finished yet
    
    * Added more to my TODO comment
    
    * Fix layout on desktop. Added z-50 index to dropdown menu
    
    * Made prep for WeatherForecastLayout body
    
    * removed unnecessary code, was used for testing
    
    * Removed comment

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="text-white">
+        <div class="my-3 text-white">
+            <h1 class="px-2 text-center text-5xl font-bold">How's the sky looking today?</h1>
+        </div>
+        <div class="mx-auto mt-10 flex flex-col gap-2 md:w-1/2 md:flex-row">
+            <span class="relative w-full md:w-4/5">
+                <input
+                    type="text"
+                    name="getCity"
+                    placeholder="Search for a place..."
+                    @keyup.enter="weatherStore.fetchWeather(getCity)"
+                    v-model="getCity"
+                    class="w-full rounded-md bg-neutral-700 py-2.5 pr-5 pl-10 placeholder:text-neutral-200 focus:outline-none"
+                />
+                <img
+                    src="/src/assets/images/icon-search.svg"
+                    alt="search-icon"
+                    class="absolute top-1/2 left-0 ml-3 w-4.5 -translate-y-1/2"
+                />
+            </span>
+            <button
+                type="button"
+                @click="weatherStore.fetchWeather(getCity)"
+                class="w-full cursor-pointer rounded-md bg-blue-500 p-2.5 md:w-1/5"
+            >
+                Search
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useWeatherStore } from '../stores/weather-store';
+
+const getCity = ref('');
+
+const weatherStore = useWeatherStore();
+</script>
