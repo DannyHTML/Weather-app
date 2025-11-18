f8ac5498612b5f1e7e2aaaf623aa9d9657765e5f
Danny <dannyjag@gmail.com>
2025-11-18 16:10:45 +0100

    Made a reuseable card layout. Made the design, also preparing the reuseable card to be used in the next design, the Daily forecast

--- src/components/WeatherForecastLayout.vue
+++ src/components/WeatherForecastLayout.vue
@@ -8,6 +8,29 @@
             :todayTemperature="weatherStore.currentWeatherTemp"
             :temperatureUnit="weatherStore.currentWeatherUnit"
         />
+
+        <div class="my-3 grid grid-cols-2 gap-3 sm:grid-cols-4">
+            <ExtraWeatherInfoLayout
+                v-for="data in dummyData"
+                :key="data.id"
+                :title="data.title"
+                :currentWeatherAmount="data.currentWeatherAmount"
+                :weatherUnit="data.weatherUnit"
+            />
+        </div>
+
+        <div>test</div>
+
+        <div class="my-3 grid grid-cols-3 gap-3 sm:grid-cols-7">
+            <ExtraWeatherInfoLayout
+                v-for="data in dummyData"
+                :key="data.id"
+                :title="data.title"
+                :currentWeatherAmount="data.currentWeatherAmount"
+                :weatherUnit="data.weatherUnit"
+                :image="data.image"
+            />
+        </div>
     </div>
 </template>
 
@@ -16,6 +39,36 @@ import { computed } from 'vue';
 import MainWeatherForecast from './MainWeatherForecast.vue';
 import { useWeatherStore } from '../stores/weather-store';
 import { useFormattedDate } from '../composables/useFormattedDate';
+import ExtraWeatherInfoLayout from './ExtraWeatherInfoLayout.vue';
+
+const dummyData = [
+    {
+        title: 'Feels like',
+        currentWeatherAmount: 18,
+        weatherUnit: '°',
+        image: '/src/assets/images/icon-rain.webp',
+        id: 1,
+    },
+    {
+        title: 'Humidity',
+        currentWeatherAmount: 46,
+        weatherUnit: '%',
+        image: '/src/assets/images/icon-sunny.webp',
+        id: 2,
+    },
+    {
+        title: 'Wind',
+        currentWeatherAmount: 14,
+        weatherUnit: 'km/h',
+        id: 3,
+    },
+    {
+        title: 'precipitation',
+        currentWeatherAmount: 0,
+        weatherUnit: 'mm',
+        id: 4,
+    },
+];
 
 const weatherStore = useWeatherStore();
 const { formatDate } = useFormattedDate();
